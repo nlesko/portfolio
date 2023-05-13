592af31f0fce7c3effdee53547b4e425a9fe838c
Nikola Leško <nlesko94@gmaill.com>
2023-05-13 09:26:14 +0200

    Modify feature cards

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -45,12 +45,13 @@ const FeatureCard: React.FC<Props> = ({ feature, ...props }) => {
         variants={introFeatureCardVariants}
         >
             <Card className="bg-steel-blue-800/30 px-8 py-12 shadow-md">
-                <div className="flex items-center justify-center h-24">
-                    {feature.icon ? React.createElement(feature.icon, { className: 'w-14 h-14' }) : null }
+                <div className="flex items-center justify-center">
+                    {feature.icon ? React.createElement(feature.icon, { className: 'w-7 h-7' }) : null }
+                    <h4 className='ml-4 text-xl'>{feature.name}</h4>
                 </div>
                 <div className="my-2" />
                 <div className='text-center'>
-                    <h4 className='text-xl'>{feature.name}</h4>
+                    
                     <div className="my-2" />
                     <p className="text-base">
                         {feature.description}
