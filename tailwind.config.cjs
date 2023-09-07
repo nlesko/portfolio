7c102e3bf52a1ae3788b2846fd6da80e9ea0c5c4
Nikola Leško <nlesko94@gmaill.com>
2023-09-08 00:01:07 +0200

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		container: {
+			center: true,
+			screens: {
+				'2xl': '1280px',
+			}
+		},
+		extend: {
+			colors: {
+				'steel-blue': {
+				  50: '#dff1ff',
+				  100: '#c0dae9',
+				  200: '#a1bfd2',
+				  300: '#6890a9',
+				  400: '#6890a9',
+				  500: '#4f7d97',
+				  600: '#426e86', // steele blue
+				  700: '#335a6f',
+				  800: '#26475a',
+				  900: '#143242',
+				}
+			  }
+		},
+	},
+	plugins: [],
+}
