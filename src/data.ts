d3562485c96f86c6154599048424a43c975a88c5
Nikola Leško <nlesko94@gmaill.com>
2024-09-11 00:11:43 +0200

    Update projects

--- src/data.ts
+++ src/data.ts
@@ -154,27 +154,17 @@ export const tools: Array<Tool> = [
 
   export const projects: Array<Project> = [
     {
+      id: 1,
+      name: "Hangman",
+      description: "A simple Hangman game made with React. The game asks you to guess the word by selecting letters from the alphabet. The game features a hint button that reveals a letter in the word.",
+      imageUrl: hangmanGameImage,
+      repository: "https://github.com/nlesko/hangman-game",
+      demoUrl: "https://hangman-game-mu-beige.vercel.app",
+      tags: ['React', 'Tailwind CSS', 'Redux'],
+      projectCategory: ProjectCategory.PERSONAL
+    },
+    {
       id: 2,
-      name: "Guess the Color Game",
-      description: "A simple 'Guess the Color' game made with JavaScript. The game asks you to guess the color corresponding to the RGB value shown in the header.",
-      imageUrl: guessTheColorGameImage,
-      repository: "https://github.com/nlesko/Guess_the_Color_Portfolio",
-      demoUrl: "http://nlesko.github.io/projects/guess-the-color-game/",
-      tags: ['JavaScript', 'CSS'],
-      projectCategory: ProjectCategory.PERSONAL
-    },
-    {
-      id: 3,
-      name: "Christmas Countdown Clock",
-      description: "In the holiday spirit, I decided to create a simple Christmas countdown clock. It features a countdown timer that tracks the time remaining until December 25, 00:00:01 AM. Additionally, I added a simple snow-like background using canvas. And what kind of Christmas would it be without some festive lights? :",
-      imageUrl: xmasCountDownClockImage,
-      repository: "https://github.com/nlesko/christmas-countdown-clock",
-      demoUrl: "https://nlesko.github.io/projects/christmas-countdown-clock/",
-      tags: ['JavaScript', 'CSS', 'canvas'],
-      projectCategory: ProjectCategory.PERSONAL
-    },
-    {
-      id: 4,
       name: "Cryptocurrency Tracker",
       description: "A cryptocurrency tracker built with vanilla JavaScript using the Coinranking API. The web application lists 50 cryptocurrencies and displays their icon, name, current worth in US dollars, and the change in value over the last 24 hours.",
       imageUrl: cryptoCurrencyTrackerImage,
@@ -184,32 +174,23 @@ export const tools: Array<Tool> = [
       projectCategory: ProjectCategory.PERSONAL
     },
     {
-      id: 5,
-      name: "Vue Smoothies",
-      description: "A simple CRUD Smoothie recipe website using Vue.js, Axios for AJAX calls and Firebase for storing data.",
-      imageUrl: cryptoCurrencyTrackerImage,
-      demoUrl: "https://serene-shore-38026.herokuapp.com/",
-      tags: ['React', 'Node', 'CSS'],
+      id: 3,
+      name: "Guess the Color Game",
+      description: "A simple 'Guess the Color' game made with JavaScript. The game asks you to guess the color corresponding to the RGB value shown in the header.",
+      imageUrl: guessTheColorGameImage,
+      repository: "https://github.com/nlesko/Guess_the_Color_Portfolio",
+      demoUrl: "http://nlesko.github.io/projects/guess-the-color-game/",
+      tags: ['JavaScript', 'CSS'],
       projectCategory: ProjectCategory.PERSONAL
     },
     {
-      id: 6,
-      name: "Build a Robot",
-      description: "A Robot builder web application that uses Vue.js, Vuex for state managment, Axios for AJAX calls and Express for serving application.",
-      imageUrl: cryptoCurrencyTrackerImage,
-      repository: "https://github.com/nlesko/build-a-bot",
-      demoUrl: "https://dry-taiga-74990.herokuapp.com/build",
-      tags: ['Vue', 'Vuex'],
-      projectCategory: ProjectCategory.PERSONAL
-    },
-    {
-      id: 7,
-      name: "Hangman",
-      description: "A simple Hangman game made with React. The game asks you to guess the word by selecting letters from the alphabet. The game features a hint button that reveals a letter in the word.",
-      imageUrl: hangmanGameImage,
-      repository: "https://github.com/nlesko/hangman-game",
-      demoUrl: "https://nlesko.github.io/projects/hangman-game/",
-      tags: ['React', 'Tailwind CSS', 'Redux'],
+      id: 4,
+      name: "Christmas Countdown Clock",
+      description: "In the holiday spirit, I decided to create a simple Christmas countdown clock. It features a countdown timer that tracks the time remaining until December 25, 00:00:01 AM. Additionally, I added a simple snow-like background using canvas. And what kind of Christmas would it be without some festive lights? :",
+      imageUrl: xmasCountDownClockImage,
+      repository: "https://github.com/nlesko/christmas-countdown-clock",
+      demoUrl: "https://nlesko.github.io/projects/christmas-countdown-clock/",
+      tags: ['JavaScript', 'CSS', 'canvas'],
       projectCategory: ProjectCategory.PERSONAL
     },
     {
