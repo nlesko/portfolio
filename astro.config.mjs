c2ea445c12a7333d046cebcb9d100dcab7e840a1
Nikola Leško <nlesko94@gmaill.com>
2024-09-16 23:28:53 +0200

    Improve SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://nikolalesko.com',
   integrations: [tailwind(), react(), icon()]
 });
